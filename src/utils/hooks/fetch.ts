dc33b7b49a1a8dc41ecced203449ebab26b2b2ab
Dennis Okparaocha <dennis_okpa@hotmail.co.uk>
2022-01-17 00:33:29 +0000

    implemented the infinte scroll

--- src/utils/hooks/fetch.ts
+++ src/utils/hooks/fetch.ts
@@ -1,26 +1,46 @@
 import { useState, useEffect } from 'react'
 
+const pageSize = 42
+
+const fetchData = async (setData: any, page: number) => {
+  const response = await fetch(
+    `https://tech-test-service-staging.seenit.studio/v1/uploads?page=${page}&perPage=${pageSize}`,
+    {
+      headers: {
+        Authorization: 'Basic dennis_okpa@hotmail.co.uk',
+      },
+    }
+  )
+
+  const data = await response.json()
+
+  setData(data)
+}
+
+const setNewContent = (content: any, data: any) => ({
+  ...data,
+  rows: [...content.rows, ...data.rows],
+})
+
 export const useFetch = () => {
-  const [content, setContent] = useState()
+  const [content, setContent] = useState({
+    rows: [],
+  })
+  const [page, setPage] = useState(1)
 
   useEffect(() => {
-    const fetchData = async () => {
-      const response = await fetch(
-        'https://tech-test-service-staging.seenit.studio/v1/uploads',
-        {
-          headers: {
-            Authorization: 'Basic dennis_okpa@hotmail.co.uk',
-          },
-        }
-      )
+    fetchData((data: any) => {
+      if (page > 1) {
+        setContent(prev => setNewContent(prev, data))
+      } else {
+        setContent(data)
+      }
+    }, page)
+  }, [page])
 
-      const data = await response.json()
+  const getMore = () => {
+    setPage(prev => prev + 1)
+  }
 
-      setContent(data || [])
-    }
-
-    fetchData()
-  }, [])
-
-  return content
+  return [content, getMore]
 }
